8a6a73ec6b814de136f3789e7078c316d7e54933
Bia1106 <54123970+Bia1106@users.noreply.github.com>
2022-09-22 10:20:47 -0300

    --

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -791,9 +791,6 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
  * Keep this code block at the end of this file to take full effect.
  */
 #
-if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-  include $app_root . '/' . $site_path . '/settings.local.php';
-}
 $settings['skip_permissions_hardening'] = FALSE;
 $databases['default']['default'] = array(
   'database' => 'drupal_project',
@@ -810,3 +807,7 @@ $settings['config_sync_directory'] = '../config/sync';
 $settings['trusted_host_patterns'] = [
   '',
 ];
+
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
+}
